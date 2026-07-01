1d3c124e34301d4f7c6788e65e85867149bbb523
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-07-02 01:51:25 +0530

    Time: 46 ms (58.98%), Space: 24.5 MB (51.56%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,21 +1,39 @@
+# class Solution:
+#     def longestSubarray(self, nums: List[int]) -> int:
+#         left,right = 0,0
+#         delete = 1
+#         out = 0
+#         while right < len(nums):
+#             if (nums[right] == 1):
+#                 out = max(out,right-left+delete)
+#             else:
+#                 if delete != 0:
+#                     point = right + 1
+#                     delete -= 1 
+#                 else:
+#                     delete = 1
+#                     left,right = point,point
+#                     continue
+#             right += 1
+#         if out == len(nums):
+#             return out - 1
+#         else:
+#             return out
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        left,right = 0,0
-        delete = 1
-        out = 0
-        while right < len(nums):
-            if (nums[right] == 1):
-                out = max(out,right-left+delete)
-            else:
-                if delete != 0:
-                    point = right + 1
-                    delete -= 1 
-                else:
-                    delete = 1
-                    left,right = point,point
-                    continue
-            right += 1
-        if out == len(nums):
-            return out - 1
-        else:
-            return out
+        left = 0
+        zeros = 0
+        ans = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeros += 1
+
+            while zeros > 1:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
+
+            ans = max(ans, right - left)
+
+        return ans
