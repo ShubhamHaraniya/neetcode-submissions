7fa493ddc64a45bfabb58afb523a01da9237bb3a
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-07-01 17:22:10 +0530

    Time: 4 ms (14.59%), Space: 19.2 MB (94.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,17 +1,5 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        # n = len(str2)
-        # m = len(str1)
-        # while n > 0:
-        #     div = int(m / n)
-        #     if div == 0:
-        #         n = n // 2
-        #         continue
-        #     if (str2 == str2[0:n]*(len(str2)//n)) and (str1 == str2[0:n]*(len(str1)//n)):
-        #         return str2[:n]
-        #     else:
-        #         n = n // 2
-        # return ""
         a = len(str2)
         b = len(str1)
         for i in range(min(a, b), 0, -1):
@@ -21,3 +9,4 @@ class Solution:
             else:
                 continue
         return ""
+        
