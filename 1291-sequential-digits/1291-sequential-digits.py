9efb01b3b8a1ebe510ebd81010b691270cee73f3
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-07-13 13:02:46 +0530

    Time: 0 ms (100%), Space: 19.3 MB (30.59%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,12 +1,26 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        ans = []
-        s = "123456789"
+        if low > high:
+            return []
 
-        for length in range(len(str(low)), len(str(high)) + 1):
-            for i in range(10 - length):
-                num = int(s[i:i + length])
+        out = []
+        n = len(str(low))
+
+        while n <= len(str(high)):
+            start = "".join(str(i) for i in range(1, n + 1))
+
+            while True:
+                num = int(start)
+
                 if low <= num <= high:
-                    ans.append(num)
+                    out.append(num)
 
-        return ans
+                # Can't generate the next sequential number
+                if start[-1] == '9':
+                    break
+
+                start = start[1:] + str(int(start[-1]) + 1)
+
+            n += 1
+
+        return out
