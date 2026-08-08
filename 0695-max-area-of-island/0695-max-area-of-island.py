7f9014a042ab4fa1434baf8060f339995fdde0af
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-08-08 16:14:37 +0530

    Time: 15 ms (84.38%), Space: 20.2 MB (67.48%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,20 +1,31 @@
 class Solution(object):
     def maxAreaOfIsland(self, grid):
-        seen = set()
-        ans = 0
-        for r0, row in enumerate(grid):
-            for c0, val in enumerate(row):
-                if val and (r0, c0) not in seen:
-                    shape = 0
-                    stack = [(r0, c0)]
-                    seen.add((r0, c0))
-                    while stack:
-                        r, c = stack.pop()
-                        shape += 1
-                        for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
-                            if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])
-                                    and grid[nr][nc] and (nr, nc) not in seen):
-                                stack.append((nr, nc))
-                                seen.add((nr, nc))
-                    ans = max(ans, shape)
-        return ans
+
+        n = len(grid)
+        m = len(grid[0])
+
+        mx = 0
+        curr = 0
+
+        def dfs(x, y):
+            nonlocal curr, mx
+
+            if x >= n or y >= m or x < 0 or y < 0 or grid[x][y] != 1:
+                mx = max(mx, curr)
+                return
+
+            curr += 1
+            grid[x][y] = 0
+
+            dfs(x + 1, y)
+            dfs(x, y + 1)
+            dfs(x - 1, y)
+            dfs(x, y - 1)
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    curr = 0
+                    dfs(i, j)
+
+        return mx
