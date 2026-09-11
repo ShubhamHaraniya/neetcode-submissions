84ba70c59b8b0ca5fe2e2b18040d6680e58813a7
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-09-12 02:03:17 +0530

    Time: 0 ms (100%), Space: 19.7 MB (63.4%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,17 +1,15 @@
 from collections import deque
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        q = deque(rooms[0])
-        v = set()
+        q = deque([0])
+        v = {0}
 
         while q:
             c = q.popleft()
-            if c not in v:
-                q.extend(rooms[c])
-            v.add(c)
-        
-        for k in range(1,len(rooms)):
-            if k not in v:
-                return False
-        
-        return True
+
+            for key in rooms[c]:
+                if key not in v:
+                    v.add(key)
+                    q.append(key)
+
+        return len(v) == len(rooms)
