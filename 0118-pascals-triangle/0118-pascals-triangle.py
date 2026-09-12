6d78317dae2c44649d0cfd757ea685ca543d47e0
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-09-12 20:53:03 +0530

    Time: 0 ms (100%), Space: 19.4 MB (29.98%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -2,12 +2,6 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         ans = []
         for i in range(numRows):
-            if i == 0:
-                ans.append([1])
-                continue
-            if i == 1:
-                ans.append([1,1])
-                continue
             temp = []
             for j in range(i+1):
                 if j == 0 or j == i:
