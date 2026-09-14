6770ac19a26f16607fcec691bc8f3d86e1dad3af
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-09-14 14:24:50 +0530

    Time: 23 ms (21.98%), Space: 21.8 MB (80.68%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -5,8 +5,7 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == 1:
                 temp += 1
-            else:
                 l = max(l,temp)
+            else:
                 temp = 0
-        l = max(l,temp)
         return l
