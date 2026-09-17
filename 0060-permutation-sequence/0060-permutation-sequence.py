b0ca694dc5b5bf7891270a23aa252b3eddaf8dfc
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-09-18 02:11:10 +0530

    Time: 0 ms (100%), Space: 19.7 MB (6.95%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,23 +1,25 @@
+from math import factorial
+
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        # fact = 1
-        # temp = n-1
-        # while temp > 0:
-        #     fact *= temp
-        #     temp -= 1
-        
-        # digit = k // fact
-        # rank = (k % fact) - 1
-
-        # def rec()
-        from math import factorial 
-        digits = list(range(1, n+1))
-        k -= 1       # 0-indexed
+        digits = list(range(1, n + 1))
+        k -= 1
         result = []
-        for i in range(n, 0, -1):
+
+        def rec(i, k):
+            if i == 0:
+                return
+
             f = factorial(i - 1)
-            idx = k // f 
-            result.append(str(digits[idx])) 
+            idx = k // f
+
+            result.append(str(digits[idx]))
             digits.pop(idx)
-            k %= f 
+
+            k %= f
+
+            rec(i - 1, k)
+
+        rec(n, k)
+
         return ''.join(result)
