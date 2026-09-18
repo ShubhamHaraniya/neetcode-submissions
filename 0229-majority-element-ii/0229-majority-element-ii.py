de90aa2a01799073d192e0c61e933d8a7fd859cf
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-09-18 11:46:35 +0530

    Time: 3 ms (92.92%), Space: 22.7 MB (51.15%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,11 +1,21 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> list[int]:
-        c1, c2, cnt1, cnt2 = None, None, 0, 0
-        for n in nums:
-            if n == c1: cnt1 += 1
-            elif n == c2: cnt2 += 1
-            elif cnt1 == 0: c1, cnt1 = n, 1
-            elif cnt2 == 0: c2, cnt2 = n, 1
-            else: cnt1 -= 1; cnt2 -= 1
+        c1 , c2 = None,None
+        p1,p2 = 0,0
 
-        return [x for x in (c1, c2) if nums.count(x) > len(nums) // 3]
+        for num in nums:
+            if num == c1:
+                p1 += 1
+            elif num == c2:
+                p2 += 1
+            elif p1 == 0:
+                c1 = num
+                p1 += 1
+            elif p2 == 0:
+                c2  = num
+                p2 += 1
+            else:
+                p1 -= 1
+                p2 -= 1
+
+        return [x for x in (c1,c2) if nums.count(x) > len(nums)//3]
