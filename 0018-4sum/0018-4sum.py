9cb1ec7ec62abd08a1d113aa4c02a0c45251e846
Shubham Haraniya <109221112+ShubhamHaraniya@users.noreply.github.com>
2026-09-18 12:13:17 +0530

    Time: 398 ms (56.85%), Space: 19.2 MB (86.16%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,25 +1,41 @@
 class Solution:
     def fourSum(self, nums: list[int], target: int) -> list[list[int]]:
-        if len(nums) < 4:
-            return []
         nums.sort()
         result = []
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                s2 = nums[i] + nums[j]
-                rei = target - s2
-                low,high = j+1,len(nums)-1
+
+        for i in range(len(nums) - 3):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, len(nums) - 2):
+
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                low = j + 1
+                high = len(nums) - 1
+
                 while low < high:
-                    if nums[low] + nums[high] == rei:
-                        if [nums[i], nums[j], nums[low], nums[high]] in result:
+                    total = nums[i] + nums[j] + nums[low] + nums[high]
+
+                    if total == target:
+                        result.append(
+                            [nums[i], nums[j], nums[low], nums[high]]
+                        )
+
+                        while low < high and nums[low] == nums[low + 1]:
                             low += 1
+
+                        while low < high and nums[high] == nums[high - 1]:
                             high -= 1
-                            continue
-                        result.append([nums[i], nums[j], nums[low], nums[high]])
+
                         low += 1
                         high -= 1
-                    elif nums[low] + nums[high] > rei:
-                        high -= 1
+
+                    elif total < target:
+                        low += 1
                     else:
-                        low += 1
+                        high -= 1
+
         return result
